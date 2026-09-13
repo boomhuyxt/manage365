97e34e284eb82c94cf5598f3eab32530dbeeb8d0
giahuy <boomhuyxt@gmail.com>
2026-09-13 22:19:30 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using manage365.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace manage365.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
